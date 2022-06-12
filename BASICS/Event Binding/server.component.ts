301f26cb10c89652280fbb50a9e45eca232d5cbe
Rupesh Keesaram <75531784+RupeshKeesaram@users.noreply.github.com>
2022-06-12 11:09:04 +0530

    Create server.component.ts

---
+++ BASICS/Event Binding/server.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  allowNewServer:boolean=true;
+
+  serverCreation:String="Server was not created !";
+
+  constructor() {
+
+    // The Serer button will get disabled after 2000 milliseconds
+
+    setTimeout(()=>{
+      this.allowNewServer=false;
+    },20000)
+   }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer():void{
+    this.serverCreation="Server Got Created!";
+  }
+}
